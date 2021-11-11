20c3cb5a6b1a3910e2ea6b376b93422026107a5c
phpcoinn <phpcoinn@gmail.com>
2021-11-11 18:23:31 +0100

    Configured wallet public key

--- apps.functions.php
+++ apps.functions.php
@@ -10,7 +10,7 @@ if($_config['testnet']) {
 	define("APPS_REPO_SERVER","https://repo..phpcoin.net");
 	define("APPS_REPO_SERVER_PUBLIC_KEY","PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSCyHWjnG15LHdWRRbNEmAPiYcyCqFZm1VKi8QziKYbMtrXUw8rqhrS3EEoyJxXASNZid9CsB1dg64u5sYgnUsrZg7C");
 	define("APPS_WALLET_SERVER_NAME","wallet.phpcoin.net");
-	define("APPS_WALLET_SERVER_PUBLIC_KEY","");
+	define("APPS_WALLET_SERVER_PUBLIC_KEY","PZ8Tyr4Nx8MHsRAGMpZmZ6TWY63dXWSD1SfcgMjJaHdx6p3BT9Jft9NXyh57c1EnisRgbtLDzXfEwddq6i717adJKkw82HMJDfRD8PZikraaq1HnBdCVxLsY");
 }
 
 
