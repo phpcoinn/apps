b667195ba2953182b23f8bbbb673cdee95fd4f35
phpcoinn <phpcoinn@gmail.com>
2022-07-21 20:52:12 +0200

    Added GA tag

--- common/include/top.php
+++ common/include/top.php
@@ -119,6 +119,18 @@ usort($menuPeers, function($p1, $p2) {
             overflow-y: auto;
         }
     </style>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-0PW26G05V9"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-0PW26G05V9');
+    </script>
+
+
 </head>
 
 
